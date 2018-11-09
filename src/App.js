85a43b3247e806dc5f6d1768810e3af484bfb330
nanlei <36439285+nanlei2000@users.noreply.github.com>
2018-11-09 18:09:12 +0800

    env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import LabelDom from '@/pages/LabelDom'
+import LabelCanvas from '@/pages/LabelCanvas'
+import { Route, Redirect, Switch } from 'react-router-dom'
 export default class App extends Component {
   render() {
     return <LabelDom />
